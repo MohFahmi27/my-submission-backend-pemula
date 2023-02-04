2b9be7ffcfad2c887421739a3cc8c356e78e90de
MohFahmi27 <mohammadfahmi417@gmail.com>
2023-02-04 09:32:05 +0800

    change status on generic error in add-book.js 🪛

--- src/handler/add-book.js
+++ src/handler/add-book.js
@@ -67,7 +67,7 @@ const addBookHandler = (request, h) => {
   }
 
   const response = h.response({
-    status: 'fail',
+    status: 'error',
     message: 'Buku gagal ditambahkan'
   })
   response.code(500)
